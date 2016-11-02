731e7f4d4b5824f569176ceb6bce0cb44e6b86c3
Alexis Reigel <mail@koffeinfrei.org>
2016-11-02 21:35:12 +0100

    root controller with links to other controllers

---
+++ app/controllers/root_controller.rb
@@ -0,0 +1,17 @@
+class RootController < ApplicationController
+  def index
+    render json: controller_entries
+  end
+
+  private
+
+  def controller_entries
+    [:code, :staff, :salary].map do |controller|
+      controller_entry(controller)
+    end.to_h
+  end
+
+  def controller_entry(controller)
+    ["#{controller}_url", url_for(controller: controller, action: :show)]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resource :code, only: [:show], controller: 'code'
   resource :staff, only: [:show], controller: 'staff'
   resource :salary, only: [:show], controller: 'salary'
+
+
+  root 'root#index'
 end
