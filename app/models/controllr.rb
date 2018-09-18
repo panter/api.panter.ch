76407d8c61c2795af7fdca4333d93fce433ef69b
Alexis Reigel <mail@koffeinfrei.org>
2018-09-18 22:15:13 +0200

    filter out invalid distances

--- app/models/controllr.rb
+++ app/models/controllr.rb
@@ -55,7 +55,7 @@ class Controllr
 
     user_addresses.map { |address|
       Geocoder::Calculations.distance_between(address, office_address)
-    }.sort
+    }.reject(&:nan?).sort
   end
 
   def commute_durations
